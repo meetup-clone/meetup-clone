7bf3ae556f2014f1587630d96d88e51f5f995a3d
Devin Lethbridge <devinbeth@gmail.com>
2018-04-09 16:47:11 -0600

    index.js and controller

---
+++ server/controller/auth_controller.js
@@ -0,0 +1,25 @@
+module.exports = {
+    checkUser: (req, res) => {
+        if (req.user) {
+            res.status(200).send(req.user);
+        }
+        else {
+            res.status(401).send('Nice try suckaaaaaa!!!!!!');
+        }
+    },
+    redirectUser: (req, res) => {
+        req.logOut();
+        res.redirect('http://localhost:3000/');
+    },
+    createUser: (req, res) => {
+        const db = req.app.get('db');
+        db.users.update({ id: req.user.id }, req.body).then(user => {
+            if (req.user.id === user[0].id) {
+                res.status(200).send(user[0]);
+            }
+            else {
+                res.status(401).send('Nice try suckaaaaaa!!!!!!');
+            }
+        });
+    }
+};
