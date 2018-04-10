9597f1620d856caadc5c44805463b179da900c40
Devin Lethbridge <devinbeth@gmail.com>
2018-04-10 15:30:00 -0600

    saving

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import logo from '../Assets/Meetup_logo.svg'
 import './Header/Header.css'
+import logo from '../Assets/Meetup_logo.svg'
+import axios from 'axios'
 
 export default class Header extends Component {
     constructor() {
         super()
         this.state = {
-
+            user: {}
         }
+    }
+    componentDidMount() {
+        axios.get('/auth/me').then(res => this.setState({ user: res.data }))
     }
     render() {
         return (
@@ -18,6 +22,8 @@ export default class Header extends Component {
                     <span>Explore</span>
                     <span>Messages</span>
                     <span>Notifications</span>
+                    <img src={this.state.user.image} alt='profile' className='profile'/>
+                    <span className='caret'>╲╱</span>
                 </section>
             </div>
         )
