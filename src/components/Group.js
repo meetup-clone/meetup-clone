ad9a80a87e17275c22aa03557a5e0212924fa4db
Devin Lethbridge <devinbeth@gmail.com>
2018-04-10 11:14:57 -0600

    stuff

---
+++ src/components/Group.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Group extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className='Group'>
+                <h1>Group</h1>
+            </div>
+        )
+    }
+}
