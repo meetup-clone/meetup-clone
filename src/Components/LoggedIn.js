bb7d569248caa61dc5b3f39f943f7112b0770f86
Devin Lethbridge <devinbeth@gmail.com>
2018-04-11 17:36:17 -0600

    event list

--- src/Components/LoggedIn.js
+++ src/Components/LoggedIn.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './LoggedIn/LoggedIn.css'
 import Header from './Header.js'
@@ -9,12 +9,20 @@ export default class LoggedIn extends Component {
         super()
         this.state = {
             myEvents: [],
-            allEvents: []
+            allEvents: [],
+            meetupsToggle: false,
+            cityToggle: false
         }
+        this.convertTime = this.convertTime.bind(this)
     }
     componentDidMount() {
         axios.get('/api/userEvents').then(res => this.setState({ myEvents: res.data }))
         axios.get('/api/allEvents').then(res => this.setState({ allEvents: res.data }))
+    }
+    convertTime(mil) {
+        let time = new Date(mil)
+        let newTime = time.toLocaleTimeString()
+        return newTime.substr(0, newTime.length - 6) + newTime.substr(newTime.length - 3, newTime.length - 1)
     }
     render() {
         let { myEvents, allEvents } = this.state;
@@ -22,12 +30,16 @@ export default class LoggedIn extends Component {
             return (
                 <div className='eventCards' key={e.event_name + e.event_id + i}>
                     <div className='eventTime'>
-                        {e.start_date}
+                        {this.convertTime(e.start_date)}
                     </div>
                     <div className='eventDetails'>
-                        <Link to={`/${e.url_name}`}>{e.group_name}</Link>
-                        <Link to={`/${e.url_name}/events/${e.event_id}`}>{e.event_name}</Link>
-                        <span>{`${e.attendees} Members going`}</span>
+                        <Link to={`/${e.url_name}`}>
+                            <span className='groupName'>{(e.group_name).toUpperCase()}</span>
+                        </Link>
+                        <Link to={`/${e.url_name}/events/${e.event_id}`}>
+                            <span className='eventName'>{e.event_name}</span>
+                        </Link>
+                            <span className='attendees'>{`${e.attendees} Members going`}</span>
                     </div>
                 </div>
             )
@@ -36,11 +48,10 @@ export default class LoggedIn extends Component {
             <div className='loggedIn'>
                 <Header />
                 <div className='nextMeetup'>
-
-                    {myEvents.length > 0 ? 
-                        <div>
+                    {myEvents.length > 0 ?
+                        <div className='eventContent'>
                             <h5>YOUR NEXT MEETUP</h5>
-                            <hr/>
+                            <hr />
                             <Link to={`/${myEvents[0].url_name}/events/${myEvents[0].event_id}`}>
                                 <h1>{myEvents[0].event_name}</h1>
                             </Link>
@@ -52,14 +63,34 @@ export default class LoggedIn extends Component {
                             <h3>{myEvents[0].venue_name}</h3>
                             <h3>{myEvents[0].venue_address}</h3>
                         </div>
-                    : 
-                        <div>
+                        :
+                        <div className='findContent'>
                             <h1>Find a Meetup</h1>
                             <h4>500 Meetups nearby</h4>
                         </div>
                     }
                 </div>
-                {eventsList}
+                <div className='filter'>
+                    <input placeholder='All Meetups' onClick={() => this.setState({ meetupsToggle: !this.state.meetupsToggle })}/>
+                    <span>within</span>
+                    <select>
+                        <option value="2">2 miles</option>
+                        <option value="5">5 miles</option>
+                        <option value="10">10 miles</option>
+                        <option value="25">25 miles</option>
+                        <option value="50">50 miles</option>
+                        <option value="100">100 miles</option>
+                        <option value="any">any distance</option>
+                    </select>
+                </div>
+                {this.state.meetupsToggle ?
+                    <div className='meetupsFilter'>
+                        
+                    </div>
+                : null}
+                <div className='events'>
+                    {eventsList}
+                </div>
             </div>
         )
     }
