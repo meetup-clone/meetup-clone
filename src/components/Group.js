d67d6b4936147c17f55287aaa22f1cf00327fe06
Taylor Miller <tmill313@gmail.com>
2018-04-11 12:17:24 -0600

    started group page layout

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Header from './Header'
+import Header from './Header';
+import './Group/Group.css'
 
 export default class Group extends Component {
     constructor(props) {
@@ -11,9 +12,22 @@ export default class Group extends Component {
 
     render() {
         return (
-            <div className='Group'>
-            <Header/>
-                <h1>Group</h1>
+            <div className='group'>
+                <Header />
+                <div className="groupCard">
+                <img src="https://secure.meetupstatic.com/photos/event/b/0/3/600_468662819.jpeg" alt="logo"/>
+                    <div className="groupInfo">
+                        <h1>Silicon Slopes</h1>
+                        <p>Lehi, UT</p>
+                        <p>members</p>
+                        <p>organizer</p>
+                        <div>
+                            <button>Join us</button>
+                            <button>...</button>
+                            <button>right arrow</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
