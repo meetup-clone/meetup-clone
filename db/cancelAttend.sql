93563cf73dbdf0066f07d80e718952786b04323e
Blair <blairslc@gmail.com>
2018-04-16 14:39:24 -0600

    Set up attending events

---
+++ db/cancelAttend.sql
@@ -0,0 +1,7 @@
+delete from attendees
+where user_id = $1 and event_id = $2;
+
+select attendees_id ,users.image, users.username from attendees
+join users on users.user_id = attendees.user_id
+where event_id = $2
+order by attendees_id;
