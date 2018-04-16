16d459ef4b9bce1eae5b780b9ec2e69468e13bdd
Devin Lethbridge <devinbeth@gmail.com>
2018-04-16 15:17:25 -0600

    saving

--- db/getUserGroupEvents.sql
+++ db/getUserGroupEvents.sql
@@ -1,7 +1,3 @@
-select events.*, groups.group_name, groups.url_name,
-(select count(event_id) from attendees where event_id =
-(select attendees.event_id from attendees where user_id = $1)) as attendees
-from events
-join groups on events.group_id = groups.group_id
-join attendees on events.event_id = attendees.event_id
-where events.event_id in (select members.group_id from members where user_id = $1)
+select groups.group_name, groups.url_name, events.*
+from events join groups on events.group_id = groups.group_id
+where groups.group_id in (select group_id from members where user_id = $1)
