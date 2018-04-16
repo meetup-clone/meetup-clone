5ba9b9377aa6843f1fef056483bc653dc77ce61c
Blair <blairslc@gmail.com>
2018-04-16 15:17:49 -0600

    Got attend and cancel to work correctly

--- db/attendEvent.sql
+++ db/attendEvent.sql
@@ -3,7 +3,7 @@ insert into attendees
 values
 ($1, $2);
 
-select attendees_id ,users.image, users.username from attendees
+select attendees_id ,users.image, users.username, users.user_id from attendees
 join users on users.user_id = attendees.user_id
 where event_id = $2
 order by attendees_id;
--- db/getAttendees.sql
+++ db/getAttendees.sql
@@ -1,4 +1,4 @@
-select attendees_id ,users.image, users.username from attendees
+select attendees_id ,users.image, users.username, users.user_id from attendees
 join users on users.user_id = attendees.user_id
 where event_id = $1
 order by attendees_id
