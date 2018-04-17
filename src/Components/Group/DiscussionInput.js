44061fe32b8d1cf647b48baa01ffb4b3c8fe7ec9
Taylor Miller <tmill313@gmail.com>
2018-04-17 12:41:01 -0600

    added blue arrow to discussion box

--- src/Components/Group/DiscussionInput.js
+++ src/Components/Group/DiscussionInput.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import dots from '../../Assets/dots.svg'
 import avatar from '../../Assets/avatar-icon.svg'
 import sendArrow from '../../Assets/send-arrow.svg'
+import blueArrow from '../../Assets/blue-arrow.svg'
 
 export default function DiscussionInput(props) {
+    console.log(props.comment)
     return (
         <div className="discussionInputMain">
-            <img src={props.user.image ? props.user.image : avatar} alt='profile' style={{ height: 40, width: 40, borderRadius: 50 }} />
-            <input maxLength="140" placeholder="Start a discussion..." />
-            <img className="sendButton" src={sendArrow} />
+            <img src={props.user.image ? props.user.image : avatar} alt='profile' style={{ height: 40, width: 40, borderRadius: 50 }}/>
+            <input maxLength="140" placeholder="Start a discussion..." onChange={e => props.typingComment(e)}/>
+            { props.comment.length ?
+            <img className="sendButton" src={blueArrow} alt="send button"/>
+            :
+            <img className="sendButton" src={sendArrow} alt="send button"/>
+            }
         </div>
     )
 }
