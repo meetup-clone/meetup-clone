a422d6f0ffc06387239eec4b9c322ae517e70f2a
Blair <blairslc@gmail.com>
2018-04-18 09:23:51 -0600

    Added blue arrow

--- src/Components/Events/EventComment.js
+++ src/Components/Events/EventComment.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import dots from '../../Assets/dots.svg'
+import blueHeart from '../../Assets/blue-heart.svg'
 
 export default function EventComment(props) {
     return (
@@ -18,7 +19,10 @@ export default function EventComment(props) {
                     <div className="replyWrapper">
                         <h5>reply</h5>
                     </div>
-                    <p># of likes</p>
+                    <div className="likeHolder">
+                        <img style={{ height: 15, width: 15 }} src={blueHeart} alt="blue heart" />
+                        <p>Like</p>
+                    </div>
                 </div>
             </div>
         </div>
